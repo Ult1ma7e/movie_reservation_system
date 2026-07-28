59ce820425b8c4e271b6cc7bcec7f0b70efb0e12
Ult1ma7e <dorin.darii111104@gmail.com>
2026-07-28 20:44:45 +0200

    chore: initial project structure

---
+++ movieReserv/src/main/java/com/example/movieReserv/MovieReservApplication.java
@@ -0,0 +1,13 @@
+package com.example.movieReserv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieReservApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieReservApplication.class, args);
+	}
+
+}
---
+++ movieReserv/src/test/java/com/example/movieReserv/MovieReservApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.movieReserv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieReservApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
